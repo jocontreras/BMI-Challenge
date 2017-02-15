a4b80781b183d6878e9bab2958b35b2cca7b5ef1
jocontreras <jen.almarasy@gmail.com>
2017-02-15 15:22:20 +0100

    added imperial method to compute for BMI

--- jasmine-standalone-2.5.2/spec/BMICalculatorSpec.js
+++ jasmine-standalone-2.5.2/spec/BMICalculatorSpec.js
@@ -7,9 +7,26 @@ describe("BMICalculator", function() {
     calculator = new BMICalculator();
   });
 
-  it("calculates BMI for a person using metric method", function() {
+  it("Calculates BMI using metric method", function() {
     calculator.metric_bmi(person);
     expect(person.bmiValue).toEqual(26.01);
   });
 
-});
+  it ("Prompts a BMI message", function(){
+    calculate.bmi(person);
+    expect(person.bmiMessage).toEqual("overweight");
+  });
+
+    });
+
+  describe ("Calculates BMI using imperial method", function(){
+      beforeEach(function(){
+        person = new Person({weight: 140, height_feet: 5, height_inches: 0});
+        calculate = new BMICalculator();
+      });
+
+      it("Calculates BMI using imperial method", function() {
+        calculator.imperial_bmi(person);
+        expect(person.bmiValue).toEqual(27.33);
+      });
+  });
--- jasmine-standalone-2.5.2/src/BMICalculator.js
+++ jasmine-standalone-2.5.2/src/BMICalculator.js
@@ -11,6 +11,18 @@ BMICalculator.prototype.metric_bmi = function(obj) {
   }
 };
 
+BMICalculator.prototype.imperial_bmi = function(obj) {
+  var weight = obj.weight_imperial;
+  var height_feet = obj.height_feet*12;
+  var height_inches = obj.height_inches;
+  var height = height_feet + height_inches;
+  if (weight > 0 && height > 0) {
+    var finalBmi = (weight * 703) / (height*height);
+    obj.bmiValue =  parseFloat(finalBmi.toFixed(2));
+    setBMIMessage(obj);
+  }
+};
+
 function setBMIMessage(obj){
   if (obj.bmiValue < 18.5) {
     obj.bmiMessage = "Underweight";
